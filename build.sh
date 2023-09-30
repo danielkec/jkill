2fefd40da0c46b6ebaaacdc51b21ff672e58cc27
Daniel Kec <daniel.kec@oracle.com>
2023-10-01 00:45:27 +0200

    Init

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/bash
-cmake --build ./cmake-build-debug --target jkill -j 18
+# sudo apt install ninja-build
+rm -rf ./target
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -G Ninja -S . -B ./target
+#cmake -B ./target --fresh
+cmake --build ./target --target jkill -j 18
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/clp/jkill
-# Build directory: /home/daniel/clp/jkill/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[TestName]=] "port_test")
-set_tests_properties([=[TestName]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/daniel/clp/jkill/CMakeLists.txt;13;add_test;/home/daniel/clp/jkill/CMakeLists.txt;0;")
